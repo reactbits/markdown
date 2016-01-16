aefe20ae79564d75a795c856d2f7f09c570da606
sergeyt <stodyshev@gmail.com>
2016-01-16 16:33:22 +0600

    updated integration of prismjs highlighter

--- src/codeblock.js
+++ src/codeblock.js
@@ -1,25 +1,6 @@
-// syntax highlighting with prism.js
-import Prism from 'prismjs-package';
-import _ from 'lodash';
-
-// language aliases
-const aliases = {
-	javascript: 'js',
-	csharp: 'c#',
-};
-
-_.toPairs(aliases).forEach(p => {
-	const lang = Prism.languages[p[0]];
-	if (!lang) return;
-	const alt = p[1];
-	if (_.isString(alt)) {
-		Prism.languages[alt] = lang;
-	} else if (_.isArray(alt)) {
-		alt.forEach(a => {
-			Prism.languages[a] = lang;
-		});
-	}
-});
+import highlight from 'prismjs-package';
+// TODO do that in prismjs-package
+import style from 'prismjs-package/themes/prism.css'; // eslint-disable-line
 
 const diagramLangs = {
 	seq: 'sequence',
@@ -34,23 +15,13 @@ function diagram(code, lang) {
 	return `<div class="diagram" data-lang="${lang}">${code}</div>`;
 }
 
-function codeBlock(html, lang) {
-	const className = `language-${lang}`;
-	return `<pre class="${className}"><code>${html}</code></pre>`;
-}
-
 export function render(code, language) {
 	const lang = (language || '').toLowerCase();
 	const diagramLang = diagramLangs[lang];
 	if (diagramLang) {
 		return diagram(code, diagramLang);
 	}
-	const grammar = Prism.languages[lang];
-	if (grammar) {
-		return codeBlock(Prism.highlight(code, grammar, lang));
-	}
-	// TODO inteligent language detection
-	return code;
+	return highlight(code, language);
 }
 
 import { unescapeAll } from 'markdown-it/lib/common/utils';

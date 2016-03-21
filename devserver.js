257a00bdfb4da0d3c8bf9520387bdff1a3c010a3
sergeyt <stodyshev@gmail.com>
2016-03-21 20:17:17 +0600

    reducing boilerplate with react-devpack

--- devserver.js
+++ devserver.js
@@ -1,35 +1,3 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const webpack = require('webpack');
-const config = require('./webpack.config');
+const startServer = require('react-devpack').startServer;
 
-const port = 8000;
-const app = express();
-const compiler = webpack(config);
-
-app.use(morgan('dev'));
-app.use(cors());
-
-app.use(require('webpack-dev-middleware')(compiler, {
-	publicPath: config.output.publicPath,
-	stats: 'errors-only',
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-
-app.use(express.static(process.cwd()));
-
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(port, '0.0.0.0', (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-
-	console.log('Listening at http://0.0.0.0:%s', port);
-});
+startServer();
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,8 @@
-const path = require('path');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const makeWebpackConfig = require('react-devpack').makeWebpackConfig;
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const cssLoader = 'css?sourceMap&modules&importLoaders=1&localIdentName=[local]';
-
-module.exports = {
-	devtool: 'source-map',
-	entry: [
-		'webpack-hot-middleware/client',
-		'./demo/index',
-	],
-	output: {
-		path: path.join(__dirname, 'static'),
-		filename: 'bundle.js',
-		publicPath: '/static/',
-	},
-	plugins: [
-		new ExtractTextPlugin('styles.css', { allChunks: true }),
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin(),
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery',
-			'window.jQuery': 'jquery',
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify(NODE_ENV),
-			},
-		}),
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.json$/,
-				loader: 'json',
-			},
-			{
-				test: /\.jsx?$/,
-				loader: 'babel',
-				exclude: /(node_modules|vendor)/,
-			},
-			{
-				test: /\.(scss|css)$/,
-				loader: ExtractTextPlugin.extract('style', [cssLoader, 'postcss', 'sass?sourceMap']),
-			},
-			{
-				test: /\.less$/,
-				loader: ExtractTextPlugin.extract('style', [cssLoader, 'postcss', 'less?sourceMap']),
-			},
-			{
-				test: /\.(ttf|eot|svg|woff(2)?)$/,
-				loader: 'file-loader',
-			},
-		],
-	},
+module.exports = makeWebpackConfig({
+	entry: './demo/index',
 	resolve: {
-		extensions: ['', '.js', '.jsx', '.json', '.css', '.scss', '.less'],
-		modulesDirectories: ['node_modules', 'demo', 'src'],
 		alias: {
 			'dev/raphael.core.js': './dev/raphael.core.js',
 			'raphael.core': './raphael.core.js',
@@ -67,5 +10,4 @@ module.exports = {
 			'raphael.vml': './dev/raphael.vml.js',
 		},
 	},
-	postcss: [autoprefixer],
-};
+});

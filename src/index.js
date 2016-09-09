0ea21cf9168e001daacc11a598c68951f8e9debb
sergeyt <stodyshev@gmail.com>
2016-09-10 00:55:17 +0700

    fix default export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Markdown } from './markdown';
+export { default } from './markdown';
 export { default as MarkdownEditor } from './editor';
 export { default as render } from './render';
 export { linkTo } from './linkify';

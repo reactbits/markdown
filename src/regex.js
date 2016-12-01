446b06a48183260dc1f13168c187e3e608c8a16e
sergeyt <stodyshev@gmail.com>
2016-12-01 21:57:18 +0700

    fixed useless escapes

--- src/regex.js
+++ src/regex.js
@@ -9,7 +9,7 @@ export const url = /^(?:(?![^:@]+:[^:@\/]*@)(http|https|ws|wss):\/\/)?((?:(([^:@
 
 // image
 export const imageExt = /\.(png|jpg|gif)$/i;
-export const flickr = /flickr.com\/[a-z]+\/[a-zA-Z@_$!\d\-\]+\/[\d]+/i;
+export const flickr = /flickr.com\/[a-z]+\/[a-zA-Z@_$!\d\-\]+/[\d]+/i;
 export const instagram = /instagram\.com\/p\/([\w\d]+)/i;
 export const slideshare = /slideshare.net\/[a-zA-Z0-9_-]*\/[a-zA-Z0-9_-]*/i;
 
@@ -17,7 +17,7 @@ export const slideshare = /slideshare.net\/[a-zA-Z0-9_-]*\/[a-zA-Z0-9_-]*/i;
 export const dailymotion = /dailymotion.com\/video\/([\w\d]+)/i;
 export const liveleak = /liveleak.com\/view\?i=[a-zA-Z0-9_]+/i;
 export const ted = /ted.com\/(.+)/i;
-export const ustream = /ustream.tv\/[a-z\/0-9]*/i;
+export const ustream = /ustream.tv\/[a-z/0-9]*/i;
 export const vimeo = /vimeo.com\/(\d+)/i;
 export const vine = /vine.co\/v\/([\w\d]+)/i;
 // TODO need params
@@ -31,13 +31,13 @@ export const spotify = /spotify.com\/track\/[a-zA-Z0-9_]+/i;
 export const googleMap = /google\.com\/maps\/embed\?(.+)/i;
 
 // code
-export const github = /github.com\/([a-zA-Z0-9\.-]+)\/([a-zA-Z0-9\.-]+)/i;
+export const github = /github.com\/([a-zA-Z0-9.-]+)\/([a-zA-Z0-9.-]+)/i;
 export const gist = /gist.github.com\/[a-zA-Z0-9_-]+\/([a-zA-Z0-9]+)/i;
 export const codepen = /codepen.io\/([A-Za-z0-9_]+)\/pen\/([A-Za-z0-9_]+)/i;
 export const ideone = /ideone.com\/[a-zA-Z0-9]{6}/i;
 export const jsbin = /jsbin.com\/[a-zA-Z0-9_]+\/[0-9_]+/i;
-export const jsfiddle = /jsfiddle.net\/[a-zA-Z0-9_]+\/[a-zA-Z0-9_\/]+/i;
-export const plunker = /plnkr.co\/edit\/[a-zA-Z0-9\?=]+/i;
+export const jsfiddle = /jsfiddle.net\/[a-zA-Z0-9_]+\/[a-zA-Z0-9_/]+/i;
+export const plunker = /plnkr.co\/edit\/[a-zA-Z0-9?=]+/i;
 
 // social media
 export const twitter = /twitter\.com\/\w+\/\w+\/\d+/i;
